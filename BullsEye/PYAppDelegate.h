f0459ebdb1f8bf9a17612d9623d468922f962cbc
Paula Yuan <pyuan@redwood.cs.hmc.edu>
2014-09-03 16:41:47 -0700

    Initial Commit

---
+++ BullsEye/PYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PYAppDelegate.h
+//  BullsEye
+//
+//  Created by Paula Yuan on 9/3/14.
+//  Copyright (c) 2014 Paula Yuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
