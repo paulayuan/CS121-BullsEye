9041506aa66db626ac75cf7a2fb203ad5fda641b
Paula Yuan <pyuan@redwood.cs.hmc.edu>
2014-09-04 11:34:09 -0700

    Added score and round functionality. Done.

--- BullsEye/PYViewController.m
+++ BullsEye/PYViewController.m
@@ -16,6 +16,8 @@
 {
     int _currentValue;
     int _targetValue;
+    int _score;
+    int _round;
 }
 
 - (void)viewDidLoad
@@ -28,6 +30,7 @@
 
 - (void)startNewRound
 {
+    _round += 1;
     _targetValue = 1 + arc4random_uniform(100);
     self.slider.value = _currentValue;
 }
@@ -35,6 +38,8 @@
 - (void)updateLabels
 {
     self.targetLabel.text = [NSString stringWithFormat:@"%d", _targetValue];
+    self.scoreLabel.text = [NSString stringWithFormat:@"%d", _score];
+    self.roundLabel.text = [NSString stringWithFormat:@"%d", _round];
 }
 
 - (void)didReceiveMemoryWarning
@@ -45,8 +50,12 @@
 
 - (IBAction)showAlert
 {
+    int difference = abs(_targetValue - _currentValue);
+    int points = 100 - difference;
+    _score += points;
+    
     NSString *message = [NSString stringWithFormat:
-                         @"The value of the slider is: %d\nThe target value is: %d", _currentValue, _targetValue];
+                         @"You scored %d points", points];
     
     UIAlertView *alertView = [[UIAlertView alloc]
                               initWithTitle:@"Hello, World"
